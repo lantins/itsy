c1a95306c3d88a6f64b2e11dd960d377db67d784
Luke Antins <lantins@gmail.com>
2008-11-17 20:37:07 +0000

    Added a test for itsy_validate.

--- lib/itsy/itsy_validate.class.php
+++ lib/itsy/itsy_validate.class.php
@@ -12,41 +12,52 @@
  * Helping you validate user input.
  * @package itsy
  */
-class itsy_validate extends itsy_error
+class itsy_validate
 {
+  function __construct()
+  {
+    $this->errors = new itsy_error();
+  }
+  
   // alphabetical characters
   public function alpha($string)
   {
-    
+    return $this;
   }
   
   // numbers, negative and decimal numbers allowed
   public function numeric($number)
   {
-    
+    return $this;
   }
   
   // alphabetical characters and numbers only
   public function alpha_numeric($string)
   {
-    
+    return $this;
   }
   
   // digits only (whole numbers) (no dots or dashes)
   public function digit($digit)
   {
-    
+    return $this;
   }
   
   // letters, numbers, whitespace, dashes, periods, and underscores
   public function text($text)
   {
-    
+    return $this;
+  }
+  
+  public function required()
+  {
+    return $this;
   }
   
   public function regex($regex)
   {
-    
+    // eregi
+    return $this;
   }
 }
 
--- test/itsy_validate/basic.test.php
+++ test/itsy_validate/basic.test.php
@@ -4,7 +4,33 @@ class test_itsy_validate_basic extends PHPUnit_Framework_TestCase
 {
   public function test_basic()
   {
-    $this->markTestIncomplete('This test has not been implemented yet.');
+    
+    // set our attributes and values
+    $input_array = array(
+      'name' => 'luke',
+      'age' => 23,
+      'occupation' => 'developer'
+      );
+    
+    $input_object = (object) $input_array;
+    
+    $input = new itsy_validate($input_object);
+    $input->name->required('may not be blank')
+                ->length(3, 16)
+                ->text();
+                
+    $input->age->required()
+                ->length(1, 3)
+                ->digit();
+                
+    $input->occupation->length(0, 32)
+                      ->regex('^[a-z0-9_\ \.-]{3,16}$');
+    
+    $input->is_valid(); // returns true/false
+    $input->errors_on_all(); // returns an object of attributes and their error messages.
+    $input->errors_on('name'); // returns an object of error messages for the spesific attribute.
+    
+    $input->name->errors(); // returns an object of error messages for the spesific attribute.
   }
 }
 
