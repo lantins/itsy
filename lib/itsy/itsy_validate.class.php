202b588849a46642c0148b449cee2fcd0fe03456
Luke Antins <lantins@gmail.com>
2008-11-24 04:34:26 +0000

    Improved itsy_validate. Added requirements to the README file.

--- lib/itsy/itsy_validate.class.php
+++ lib/itsy/itsy_validate.class.php
@@ -28,6 +28,7 @@ class itsy_validate
   function __construct($attributes)
   {
     $this->attributes = $attributes;
+    $this->rules = array();
     $this->errors = new itsy_error();
   }
   
@@ -53,6 +54,48 @@ class itsy_validate
   }
   
   /**
+   * Add Validation Rule Proxy Method
+   * 
+   * This magic method will add validation rules to an attribute, if the
+   * validation call back exists. Essentially gives a nicer interface.
+   * 
+   * @param string $name of the static validation call back.
+   * @param array $arguments to be passed to the specified method.
+   * @return object this object, for method chaining.
+   * @throws itsy_exception if the call back method does not exist.
+   */
+  public function __call($name, $arguments)
+  {
+    // check if we have a matching static method.
+    $callback = "test_$name";
+    if (method_exists('itsy_validate', $callback)) {
+      $value = $this->get_current_attribute_value();
+      if ($value === false) return $this;
+      
+      $this->add_rule($this->current_attribute, $callback, $arguments);
+      
+      return $this;
+    }
+    throw new itsy_exception("Validation method: '$name' could not be found.");
+  }
+  
+  /**
+   * Add Validation Rule
+   * 
+   * Adds a validation rule for a specific attribute
+   * 
+   * @param string $attribute_name to add the rule to.
+   * @param mixed $callback to be passed to the call back method.
+   * @param array $arguments to be passed to the specified method.
+   * @return object this object, for method chaining.
+   * @throws itsy_exception if the call back method does not exist.
+   */
+  private function add_rule($attribute_name, $callback, $arguments)
+  {
+    $this->rules[$attribute_name][$callback][] = $arguments;
+  }
+  
+  /**
    * Is Valid?
    * 
    * Applies the validation rules.
@@ -61,6 +104,25 @@ class itsy_validate
    */
   public function is_valid()
   {
+    // call the validation rules for each attribute.
+    foreach ($this->rules as $attribute_name => $rules) {
+      $this->current_attribute = $attribute_name;
+      
+      $value = $this->get_current_attribute_value();
+      // we don't _always_ need to run the validation rules.
+      if (array_key_exists('test_required', $rules) == false && $value == false) {
+        continue;
+      }
+      
+      foreach ($rules as $rule => $arguments) {
+        //call_user_func("itsy_validate::$rule")
+        // lets call each rule!
+        array_unshift($arguments, $value);
+        $reflector = new ReflectionMethod($this, $rule);
+        $reflector->invokeArgs($this, $arguments);
+      } 
+    }
+    
     if ($this->errors->count() == 0) {
       return true;
     }
@@ -98,41 +160,50 @@ class itsy_validate
    * 
    * Gets the value for the 'current attribute'.
    * 
-   * @return mixed value of specified attribute, false if the current attribute is not set.
+   * @return mixed value of current attribute, false if the attribute is not set.
    */
   private function get_current_attribute_value()
   {
-    if (array_key_exists($this->current_attribute, $this->attributes)) {
-      return $this->attributes[$this->current_attribute];
+    return $this->get_attribute_value($this->current_attribute);
+  }
+  
+  /**
+   * Get Specific Attribute Value
+   * 
+   * Gets the value for a specific attribute.
+   * 
+   * @return mixed value of specified attribute, false if the attribute is not set.
+   */
+  private function get_attribute_value($attribute)
+  {
+    if (array_key_exists($attribute, $this->attributes)) {
+      return $this->attributes[$attribute];
     }
     
     return false;
   }
   
-  /**
-   * ----------------------------------------------------------------------
-   * VALIDATION RULES
-   * The below methods are used to add validation rules to an attribute.
-   * ----------------------------------------------------------------------
-   */
+  
   
   /**
    * Validate Alphabetical Characters
    * 
-   * @return object this object, for method chaining.
+   * @param mixed $value to test.
+   * @return bool false if we found an error.
    */
-  public function alpha()
+  public function test_alpha($value)
   {
-    $value = $this->get_current_attribute_value();
-    if ($value === false) return $this;
-    
-    $after = itsy_filter::alpha($value);
-    
-    if ($after != $value) {
-      $this->errors->add($this->current_attribute, "must be a alphabetical characters only.");
+    if (is_array($value)) {
+      foreach ($value as $index => $val) {
+        $this->test_alpha($val);
+      }
+    } else {
+      $after = itsy_filter::alpha($value);
+      if ($after != $value) {
+        $this->errors->add($this->current_attribute, "must be a alphabetical characters only.");
+        return false;
+      }
     }
-    
-    return $this;
   }
   
   /**
@@ -140,39 +211,22 @@ class itsy_validate
    * 
    * Numbers, negative and decimal is allowed
    * 
-   * @return object this object, for method chaining.
+   * @param mixed $value to test.
+   * @return bool false if we found an error.
    */
-  public function numeric()
+  public function test_numeric($value)
   {
-    $value = $this->get_current_attribute_value();
-    if ($value === false) return $this;
-    
-    $after = itsy_filter::numeric($value);
-    
-    if ($after != $value) {
-      $this->errors->add($this->current_attribute, "must be a valid numeric number. (negative and decimal numbers allowed)");
+    if (is_array($value)) {
+      foreach ($value as $index => $val) {
+        $this->test_numeric($val);
+      }
+    } else {
+      $after = itsy_filter::numeric($value);
+      if ($after != $value) {
+        $this->errors->add($this->current_attribute, "must be a valid numeric number. (negative and decimal numbers allowed)");
+        return false;
+      }
     }
-    
-    return $this;
-  }
-  
-  /**
-   * Validate Alphabetical Characters and Numbers
-   * 
-   * @return object this object, for method chaining.
-   */
-  public function alpha_numeric()
-  {
-    $value = $this->get_current_attribute_value();
-    if ($value === false) return $this;
-    
-    $after = itsy_filter::alpha_numeric($value);
-    
-    if ($after != $value) {
-      $this->errors->add($this->current_attribute, "must be a alphabetical characters and numbers only.");
-    }
-    
-    return $this;
   }
   
   /**
@@ -180,20 +234,84 @@ class itsy_validate
    * 
    * Digits only (whole numbers) (no dots or dashes)
    * 
-   * @return object this object, for method chaining.
+   * @param mixed $value to test.
+   * @return bool false if we found an error.
    */
-  public function digit()
+  public function test_digit($value)
   {
-    $value = $this->get_current_attribute_value();
-    if ($value === false) return $this;
-    
-    $after = itsy_filter::digit($value);
-    
-    if ($after != $value) {
-      $this->errors->add($this->current_attribute, "must be a valid digit. (whole numbers only, no dots or dashes)");
+    if (is_array($value)) {
+      foreach ($value as $index => $val) {
+        $this->test_digit($val);
+      }
+    } else {
+      $after = itsy_filter::digit($value);
+      if ($after != $value) {
+        $this->errors->add($this->current_attribute, "must be a valid digit. (whole numbers only, no dots or dashes)");
+        return false;
+      }
     }
+  }
+  
+  /**
+   * Validate Alphabetical Characters and Numbers
+   * 
+   * @param mixed $value to test.
+   * @return bool false if we found an error.
+   */
+  public function test_alpha_numeric($value)
+  {
+    if (is_array($value)) {
+      foreach ($value as $index => $val) {
+        $this->test_alpha_numeric($val);
+      }
+    } else {
+      $after = itsy_filter::alpha_numeric($value);
+      if ($after != $value) {
+        $this->errors->add($this->current_attribute, "must be a alphabetical characters and numbers only.");
+        return false;
+      }
+    }
+  }
+  
+  /**
+   * Validate Required
+   * 
+   * Makes an attribute required.
+   * 
+   * @param mixed $value to test.
+   * @return bool false if we found an error.
+   */
+  public function test_required($value)
+  {
+    if ($value == false) {
+      $this->errors->add($this->current_attribute, 'is required and may not be left empty.');
+    }
+  }
+  
+  /**
+   * Validate Length
+   * 
+   * Validates using a minimum and maximum length.
+   * 
+   * @param mixed $value to test.
+   * @param array $arguments (min length, max length)
+   * @return bool false if we found an error.
+   */
+  public function test_length($value, $arguments)
+  {
+    list($min, $max) = $arguments;
     
-    return $this;
+    if (is_array($value)) {
+      foreach ($value as $index => $val) {
+        $this->test_length($val, $arguments);
+      }
+    } else {
+      $length = strlen($value);
+      if ($length < $min || $length > $max) {
+        $this->errors->add($this->current_attribute, "length must be between or equal to $min and $max.");
+        return false;
+      }
+    }
   }
   
   /**
@@ -202,81 +320,80 @@ class itsy_validate
    * Standard text validation, letters, numbers, whitespace, dashes, periods
    * and underscores
    * 
-   * @return object this object, for method chaining.
+   * @param mixed $value to test.
+   * @return bool false if we found an error.
    */
-  public function text()
+  public function test_text($value)
   {
-    $value = $this->get_current_attribute_value();
-    if ($value === false) return $this;
-    
-    $after = itsy_filter::text($value);
-    
-    if ($after != $value) {
-      $this->errors->add($this->current_attribute, "must be standard text. (allowed: letters, numbers, spaces, dashes, periods, underscores)");
+    if (is_array($value)) {
+      foreach ($value as $index => $val) {
+        $this->test_text($val);
+      }
+    } else {
+      $after = itsy_filter::text($value);
+      if ($after != $value) {
+        $this->errors->add($this->current_attribute, "must be standard text. (allowed: letters, numbers, spaces, dashes, periods, underscores)");
+        return false;
+      }
     }
-    
-    return $this;
   }
   
   /**
    * Validate Against Regular Expression
    * 
-   * @return object this object, for method chaining.
+   * @param mixed $value to test.
+   * @param array $arguments (regex, (optional) custom error message)
+   * @return bool false if we found an error.
    */
-  public function regex($regex, $message = false)
+  public function test_regex($value, $arguments)
   {
-    $value = $this->get_current_attribute_value();
-    if ($value === false) return $this;
+    $regex = $arguments[0];
+    $message = (array_key_exists(1, $arguments)) ? $arguments[1] : false;
     
-    if (eregi($regex, $value) == false) {
-      if ($message == false) {
-        $message = 'is not valid.';
+    if (is_array($value)) {
+      foreach ($value as $index => $val) {
+        $this->test_regex($val, $arguments);
       }
-      $this->errors->add($this->current_attribute, $message);
+    } else {
+      if (eregi($regex, $value) == false) {
+        if ($message == false) {
+          $message = 'is not valid.';
+        }
+        $this->errors->add($this->current_attribute, $message);
+        return false;
+      }
     }
-    
-    return $this;
   }
   
   /**
-   * Validate Required
+   * Validate Custom Rule
    * 
-   * Makes an attribute required.
+   * Validates using the supplied call back with an optional custom error message.
    * 
-   * @return object this object, for method chaining.
+   * @param mixed $value to test.
+   * @param array $arguments (call back, (optional) custom error message)
+   * @return bool false if we found an error.
    */
-  public function required()
+  public function test_custom($value, $arguments)
   {
-    $value = $this->get_current_attribute_value();
+    $callback = $arguments[0];
+    $message = (array_key_exists(1, $arguments)) ? $arguments[1] : false;
     
-    if (strlen($value) == 0) {
-      $this->errors->add($this->current_attribute, 'is required and may not be left empty.');
+    if (is_array($value)) {
+      foreach ($value as $index => $val) {
+        $this->test_custom($val, $arguments);
+      }
+    } else {
+      $result = call_user_func($callback, $value);
+      if ($result == false) {
+        if ($message == false) {
+          $this->errors->add($this->current_attribute, 'is not valid.');
+        } elseif(is_string($message)) {
+          $this->errors->add($this->current_attribute, $message);
+        }
+        return false;
+      }
     }
-    
-    return $this;
-  }
-  
-  /**
-   * Validate Length
-   * 
-   * Makes an attribute required.
-   * 
-   * @param string $min length to allow
-   * @param string $max length to allow.
-   * @return object this object, for method chaining.
-   */
-  public function length($min, $max)
-  {
-    $value = $this->get_current_attribute_value();
-    if ($value === false) return $this;
-    
-    $length = strlen($value);
-    
-    if ($length < $min || $length > $max) {
-      $this->errors->add($this->current_attribute, "length must be between or equal to $min and $max.");
-    }
-    
-    return $this;
   }
 }
 
--- test/itsy_validate/basic.test.php
+++ test/itsy_validate/basic.test.php
@@ -11,9 +11,12 @@ class test_itsy_validate_basic extends PHPUnit_Framework_TestCase
       'name' => array('first' => 'luke', 'last' => 'antins'),
       'age' => 23,
       'occupation' => 'developer',
-      'hobbies' => array('climbing', 'coding', 'meowing'),
+      'hobbies_ok' => array('climbing', 'coding', 'meowing'),
+      'hobbies_fail' => array('toooooooooooooooolooooooonnnnnnngggggggggg', 'hissing'),
       'no_rules' => 'no rules applied to this attribute',
       'not_required' => '',
+      'foo1' => 'meow',
+      'foo2' => 'bar',
       'error_1' => '',
       'error_2' => 'should be a digit!'
       );
@@ -21,7 +24,8 @@ class test_itsy_validate_basic extends PHPUnit_Framework_TestCase
     // TODO: should be able to pass an array OR object?
     // Lets set some validation rules.
     $validate = new itsy_validate($input);
-    $validate->name->required('first', 'last')
+    
+    $validate->name->required()
                    ->length(3, 16)
                    ->text();
     
@@ -31,16 +35,36 @@ class test_itsy_validate_basic extends PHPUnit_Framework_TestCase
     
     $validate->occupation->regex('^[a-z0-9_\ \.-]{3,16}$');
     
-    $validate->hobbies->required()
-                      ->text();
+    $validate->hobbies_ok->required()
+                         ->text();
     
     // so far everything _should_ be valid.
     $this->assertEquals(true, $validate->is_valid());
+    
+    $validate->hobbies_fail->required()
+                           ->length(6, 12)
+                           ->text();
     
     // this is not required; do not apply rules if no input is given.
     $validate->not_required->length(6, 12)
                            ->text()
                            ->regex('^[a-z]{8,16}$');
+    
+    // custom validation function.
+    function does_foo_equal_bar($value)
+    {
+      if ($value == 'bar') {
+        return true;
+      }
+      
+      return false;
+    }
+    
+    $validate->foo1->length(1, 8)
+                   ->custom('does_foo_equal_bar', 'foo should be bar!');
+    
+    $validate->foo2->length(1, 8)
+                   ->custom('does_foo_equal_bar', 'foo should be bar!');
     
     $validate->error_1->required()
                       ->length(6, 12)
@@ -54,11 +78,16 @@ class test_itsy_validate_basic extends PHPUnit_Framework_TestCase
     
     $this->assertEquals(false, $validate->is_valid());
     $this->assertEquals(false, $validate->errors_on('name'));
+    $this->assertEquals(false, $validate->errors_on('age'));
+    $this->assertEquals(false, $validate->errors_on('occupation'));
+    $this->assertEquals(false, $validate->errors_on('hobbies_ok'));
     $this->assertEquals(false, $validate->errors_on('not_required'));
+    $this->assertEquals(1, count((array) $validate->errors_on('hobbies_fail')));
+    $this->assertEquals(1, count((array) $validate->errors_on('foo1')));
+    $this->assertEquals(1, count((array) $validate->errors_on('foo2')));
     $this->assertEquals(2, count((array) $validate->errors_on('error_1')));
     $this->assertEquals(2, count((array) $validate->errors_on('error_2')));
     $this->assertEquals(1, count((array) $validate->errors_on('error_3')));
-    
     #$foo = $validate->errors_on_all(); // returns an object of attributes and their error messages.
   }
 }
